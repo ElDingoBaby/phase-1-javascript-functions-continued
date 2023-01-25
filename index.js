099e2a9fff0cd0fbdf38ecfa678be3e0a6f1ae99
Alexis Sanchez <alexiss574@gmail.com>
2023-01-24 20:35:14 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// code your solution here
+
+function saturdayFun(activity) {
+    const message = `This Saturday, I want to roller-skate!`
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`
+    } else {
+        return message
+    }
+}
+
+const mondayWork = function (activity) {
+    let defaultActivity
+    const defaultMessage = `This Monday, I will go to the office.`
+    if (activity) {
+        return `This Monday, I will ${activity}.`
+    } else if (defaultActivity) {
+        return `This Monday, I will go to the office.`
+    } else {
+        return defaultMessage
+    }
+}
+
+function wrapAdjective(start, character = "Special") {
+    if (character = ``) {
+        return function (character) {
+            return `You are ${start}${character}!`
+        }
+    } else if (character = `||`) {
+        return function (character) {
+            return `You are ${start}${character}${start}!`
+        }
+    }
+}
